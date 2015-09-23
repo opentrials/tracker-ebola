31cfe98e0c3e8593022c5d4278889a9c998f4c3f
roll <roll@respect31.com>
2015-09-23 17:55:21 +0300

    added api/data func test

--- tests/func/api.js
+++ tests/func/api.js
@@ -1,29 +1,44 @@
 'use strict';
 
-// var Browser = require('zombie');
-// var app = require('../prototype/app');
-// var assert = require('chai').assert;
-// var lodash = require('lodash');
+var reqdir = require('require-dir');
+var Browser = require('zombie');
+var lodash = require('lodash');
+var assert = require('chai').assert;
+var services = reqdir('../../prototype/services');
+var app = require('../../prototype/app');
 
-// process.env.NODE_ENV = 'test';
-// Browser.localhost('127.0.0.1', process.env.PORT || 3000);
+// Prepare browser
+Browser.localhost('127.0.0.1', process.env.PORT || 3001);
 
-// before(function(done) {
-  // // Run the server
-  // app.listen(3000, function() {
-    // done();
-  // });
-// });
+// Prepare server
+before(function(done) {
 
-// describe('Core', function() {
-  // var browser = new Browser({maxWait: 5000});
-  // // Ensure we have time for request to resolve, etc.
-  // this.timeout(10000);
+  // Run the server
+  app.listen(3001, function() {
+    done();
+  });
 
-  // it('Should be alive', function (done) {
-    // browser.visit('/', function() {
-      // assert.ok(browser.success);
-      // done();
-    // });
-  // });
-// });
+});
+
+describe('Data endpoint', function() {
+
+  var browser = new Browser({maxWait: 5000});
+  this.timeout(10000);
+
+  it('Should be alive', function (done) {
+    browser.visit('/api/data', function() {
+      assert.ok(browser.success);
+      done();
+    });
+  });
+
+  it('Should follow data service return', function (done) {
+    browser.visit('/api/data', function() {
+      services.data.get().then(function (data) {
+        assert.deepEqual(JSON.parse(browser.text()), {'results': data});
+      });
+      done();
+    });
+  });
+
+});
