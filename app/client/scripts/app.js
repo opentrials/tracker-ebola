a0e0f826613be0ae6f70e75ec5e1dd17de945b09
roll <roll@respect31.com>
2015-09-28 13:53:44 +0300

    used jquery cdn, fixed js errors

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -9,7 +9,6 @@ application.controller('Controller', ['$scope', '$http', function($scope, $http)
     // Make an API call
     $http.get('/api/data').
         then(function(res) {
-            console.log(res.data);
             $scope.results = res.data.results;
         }, function(res) {
             //TODO: implement
