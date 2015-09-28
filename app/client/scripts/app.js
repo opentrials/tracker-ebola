76194abef44ca20177055e74091cbc7b0e121ec7
roll <roll@respect31.com>
2015-09-28 14:02:33 +0300

    fixed coding style

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -4,15 +4,15 @@
 var application = angular.module('Application',[]);
 
 // Root controller
-application.controller('Controller', ['$scope', '$http', function($scope, $http) {
+application.controller('Controller', ['$scope', '$http', function($scope, $http) { // jscs:disable
 
-    // Make an API call
-    $http.get('/api/data').
-        then(function(res) {
-            $scope.results = res.data.results;
-        }, function(res) {
-            //TODO: implement
-            console.log(res);
-        });
+  // Make an API call
+  $http.get('/api/data').
+    then(function(res) {
+      $scope.results = res.data.results;
+    }, function(res) {
+      //TODO: implement
+      console.log(res);
+    });
 
 }]);
