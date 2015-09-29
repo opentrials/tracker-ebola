c3b7efc6236f018008ce477530ce94b68215ed6b
roll <roll@respect31.com>
2015-09-29 16:41:05 +0300

    added trial statistics

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -6,41 +6,79 @@ var application = angular.module('Application',[]);
 // Root controller
 application.controller('Controller', ['$scope', '$http', '$interval', function($scope, $http, $interval) { // jscs:disable
 
-  // Defaults results
-  $scope.results = [];
+  // Run
+  update_model();
+  $interval(update_model, 5*60*1000); // 5 min
 
-  // Updating results
-  update();
-  $interval(update, 5*60*1000); // 5 min
-
-  function update() {
-    $scope.results = [];
+  // Update model
+  function update_model() {
+    $scope.trials = [];
     $http.get('/api/data').
       then(function(res) {
-          var results = res.data.results;
-          results.forEach(function (item) {
-
-            // Results
-            var res = item['Study Results'];
-            if (!res) {
-                res = 'No results';
-            }
-            item['_results'] = res;
-
-            // Days
-            var days = null;
-            if (item['Completion Date']) {
-              var today = new Date();
-              var compl = new Date(item['Completion Date']);
-              days = Math.floor((today - compl)/1000/60/60/24);
-            }
-            item['_days'] = (days > 0) ? days : null;
-
-          });
-          $scope.results = results;
+          var trials = res.data.results;
+          process_trials(trials);
+          $scope.trials = trials;
+          $scope.counter = count_trials(trials);
       }, function(res) {
           //TODO: implement
           console.log(res);
       });
   }
+
 }]);
+
+/**
+ * Process every trial
+ *
+ * @param {array} trials
+ */
+function process_trials(trials) {
+
+  // Iterate over trials
+  trials.forEach(function (trial) {
+
+    // Completion
+    trial['_completion'] = trial['Completion Date'];
+
+    // Results
+    trial['_results'] = trial['Study Results'] || 'No results';
+
+    // Days
+    var days = null;
+    if (trial['Completion Date']) {
+      var today = new Date();
+      var completion = new Date(trial['Completion Date']);
+      days = Math.floor((today - completion)/1000/60/60/24);
+    }
+    trial['_days'] = (days > 0) ? days : null;
+
+  });
+
+}
+
+/**
+ * Count trials
+ *
+ * @param {array} trials
+ */
+function count_trials(trials) {
+
+  var counter = {};
+
+  counter.trials = trials.length;
+  counter.completed = trials.filter(function(trial) {
+    var key = 'Completion Date';
+    return trial[key] !== null && (new Date(trial[key]) < new Date());
+  }).length;
+  counter.resulted = trials.filter(function(trial) {
+    var key = 'Study Results';
+    return trial[key] !== null;
+  }).length;
+  counter.source_list = _.uniq(_.pluck(trials, 'Source'));
+  counter.sources = counter.source_list.length;
+  counter.source_list = counter.source_list.slice(0, -1).join(', ') +
+    ' and ' + counter.source_list.slice(-1)[0];
+
+  return counter;
+
+}
